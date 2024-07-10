56be58dcc3563f3945863b66613605507e63a6ef
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-07-10 12:55:58 +0530

    Learnt about datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); 
+console.log(valueInNumber);
+
+
+// "33 => 33"
+// "33abc" => NaN
+// true = 1; false = 0
+
+let isLoggedIn = 1;
+let isLoggedIn2 = 0;
+let isLoggedIn3 = "sumeet";
+let isLoggedIn4 = "";
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+let booleanisLoggedIn2 = Boolean(isLoggedIn2);
+let booleanisLoggedIn3 = Boolean(isLoggedIn3);
+let booleanisLoggedIn4 = Boolean(isLoggedIn4);
+
+console.log(booleanisLoggedIn);
+console.log(booleanisLoggedIn2);
+console.log(booleanisLoggedIn3);
+console.log(booleanisLoggedIn4);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "sumeet" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
