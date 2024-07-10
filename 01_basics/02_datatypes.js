20f2c1eb712ccd1e9de7e088e335a00cefb832b7
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-07-10 12:40:40 +0530

    learnt about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node js, not browser
+
+
+console.log(3 + 3);
+
+console.log("Sumeet");
+
+let name = "sumeet";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null => undefined
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(null)); // null
+console.log(typeof(undefined)); //undefined
+
+
+
+
